1fe9a89114a6d3d24e95872f1de1d282a064470a
Ruslan Merdeev <rz.merdeev@itelma.su>
2017-08-28 14:01:13 +0400

    Output temperature, pressure and humidity. Fixed problem: if no internet connection - appli fall.

--- app/src/main/java/com/itelma/myapplication/MainActivity.java
+++ app/src/main/java/com/itelma/myapplication/MainActivity.java
@@ -5,74 +5,82 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Button;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 import java.lang.*;
-import java.net.URL;
-import java.util.*;
-import java.net.URLConnection;
+import java.io.*;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    public TextView text;
+    public TextView status;
     public Button button;
+    public TextView temp;
+    public TextView pres;
+    public TextView hum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        text = (TextView) findViewById(R.id.textView);
+        status = (TextView) findViewById(R.id.textView);
         button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                try {
-                    new Update().execute();
-                }
-                catch (Exception e) {
-                    text.setText("Exception:" + e.getMessage());
-                }
+                new Request().execute();
             }
         });
+        temp = (TextView) findViewById(R.id.textView2);
+        pres = (TextView) findViewById(R.id.textView3);
+        hum = (TextView) findViewById(R.id.textView4);
     }
 
-    class Update extends AsyncTask<Void, Void, Void> {
+    class Request extends AsyncTask<Void, Void, Void> {
 
-        StringBuilder resultString = new StringBuilder();
+        String[] params;
+        String result;
 
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            text.setText("Processing...please wait");
+            status.setText("requesting...");
         }
 
         @Override
-        protected Void doInBackground(Void... params) {
+        protected Void doInBackground(Void... stub) {
 
             try {
-                URL url = new URL("http://www.btlt.ru/info/temperature");
-                URLConnection conn = url.openConnection();
-                conn.connect();
-                Scanner scan = new Scanner(conn.getInputStream());
-                while (scan.hasNext()) {
-                    resultString.append(scan.next());
-                }
-                scan.close();
+//                URL url = new URL("http://www.btlt.ru/info/temperature");
+//                URLConnection conn = url.openConnection();
+//                conn.connect();
+
+                Document doc = Jsoup.connect("http://www.btlt.ru/info/temperature").get();
+                String strong = doc.body().getElementsByTag("strong").text();
+                params = strong.split(" ");
+                result = "ok";
             }
-            catch (Exception e) {
-                resultString.setLength(0);
-                resultString.append("Exception:" + e.getMessage());
+            catch (IOException e) {
+                //result = "IOException:" + e.getMessage();
+                result = "no internet connection!";
             }
 
             return null;
         }
 
         @Override
-        protected void onPostExecute(Void result) {
-            super.onPostExecute(result);
-            text.setText(resultString);
+        protected void onPostExecute(Void stub) {
+            super.onPostExecute(stub);
+            status.setText(result);
+            if( result.contains("ok") ) {
+                temp.setText(params[0]);
+                pres.setText(params[1] + "мм.рт.ст");
+                hum.setText(params[2] + "%");
+            }
         }
     }
 }
